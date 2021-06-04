a641d5e7c4d5fbd4a92c5c1fa27f98eaaff02394
julymaltceva <44710422+julymaltceva@users.noreply.github.com>
2021-06-04 13:52:17 +0300

    add mobile styles, add other products section

--- src/components/product/ProductFull.vue
+++ src/components/product/ProductFull.vue
@@ -2,66 +2,84 @@
   <div v-if="data">
     <section class="product">
       <div class="container container--wide">
-        <Breadcrumbs :breadcrumbs="data.breadcrumbs" :title="data.title"></Breadcrumbs>
+        <Breadcrumbs
+          :breadcrumbs="data.breadcrumbs"
+          :title="data.title"
+        ></Breadcrumbs>
         <div class="container">
           <div class="product__inner">
-          <div class="product__gallery">
-            <div class="product__gallery-inner">
+            <div class="product__gallery">
+              <div class="product__gallery-inner">
+                <swiper
+                  class="swiper product__swiper product__swiper--top"
+                  :options="swiperOptionTop"
+                  ref="swiperTop"
+                >
+                  <swiper-slide
+                    v-for="(image, i) in data.gallery"
+                    :key="i"
+                    class="product__item"
+                  >
+                    <div class="product__item-image">
+                      <img :src="image" />
+                    </div>
+                  </swiper-slide>
+                </swiper>
+              </div>
               <swiper
-                class="swiper product__swiper product__swiper--top"
-                :options="swiperOptionTop"
-                ref="swiperTop"
+                class="swiper product__swiper product__swiper--thumbs"
+                :options="swiperOptionThumbs"
+                ref="swiperThumbs"
               >
                 <swiper-slide
                   v-for="(image, i) in data.gallery"
                   :key="i"
                   class="product__item"
                 >
-                  <div class="product__item-image">
+                  <div class="product__item-image product__item-image--small">
                     <img :src="image" />
                   </div>
                 </swiper-slide>
               </swiper>
+              <div class="swiper-button-next" slot="button-next">
+                <svg class="icon" width="20" height="20" aria-hidden="true">
+                  <use xlink:href="#swiper-arrow"></use>
+                </svg>
+              </div>
+              <div class="swiper-button-prev" slot="button-prev">
+                <svg class="icon" width="20" height="20" aria-hidden="true">
+                  <use xlink:href="#swiper-arrow"></use>
+                </svg>
+              </div>
             </div>
-            <swiper
-              class="swiper product__swiper product__swiper--thumbs"
-              :options="swiperOptionThumbs"
-              ref="swiperThumbs"
-            >
-              <swiper-slide
-                v-for="(image, i) in data.gallery"
-                :key="i"
-                class="product__item"
-              >
-                <div class="product__item-image product__item-image--small">
-                  <img :src="image" />
-                </div>
-              </swiper-slide>
-            </swiper>
-                <div class="swiper-button-next" slot="button-next">
-                  <svg class="icon" width="20" height="20" aria-hidden="true">
-                    <use xlink:href="#swiper-arrow"></use>
-                  </svg>
-                </div>
-                <div class="swiper-button-prev" slot="button-prev">
-                  <svg class="icon" width="20" height="20" aria-hidden="true">
-                    <use xlink:href="#swiper-arrow"></use>
-                  </svg>
-                </div>
+            <div class="product__info">
+              <h2 v-html="data.author"></h2>
+              <h5 class="product__name" v-html="data.name"></h5>
+              <p class="product__description" v-html="data.options"></p>
+              <p
+                class="product__description"
+                v-for="(desc, d) in data.description"
+                :key="d"
+                v-html="desc"
+              ></p>
+              <h4 class="product__price" v-html="data.price"></h4>
+              <button
+                class="button button--black"
+                v-html="data.button"
+              ></button>
+              <a
+                class="product__author-link"
+                :href="data.author_link.link"
+                v-html="data.author_link.text"
+              ></a>
+            </div>
           </div>
-          <div class="product__info">
-            <h2 v-html="data.author"></h2>
-            <h4 class="product__name" v-html="data.name"></h4>
-            <p class="product__description" v-html="data.options"></p>
-            <p class="product__description" v-for="(desc, d) in data.description" :key="d" v-html="desc"></p>
-            <p class="product__price" v-html="data.price"></p>
-            <button class="button button--black" v-html="data.button"></button>
-            <a class="product__author-link" :href="data.author_link.link" v-html="data.author_link.text"></a>
-          </div>
-        </div>
         </div>
       </div>
     </section>
+    <OtherWorks
+      :title="data.other_title" :author="data.author"
+    ></OtherWorks>
   </div>
 </template>
 
@@ -69,39 +87,41 @@
 import Breadcrumbs from "../common/Breadcrumbs.vue";
 import { Swiper, SwiperSlide, directive } from "vue-awesome-swiper";
 import "swiper/css/swiper.css";
+import OtherWorks from "../common/OtherWorks.vue"
 
 export default {
   name: "ProductFull",
   components: {
     Swiper,
     SwiperSlide,
-    Breadcrumbs
+    Breadcrumbs,
+    OtherWorks
   },
   directives: {
-    swiper: directive
+    swiper: directive,
   },
   data() {
     return {
       currency: " ₽",
       swiperOptionTop: {
-        breakpoints: {
-          768: {
-            loop: true,
-            loopedSlides: 3,
-            slidesPerView: "auto",
-            draggable: false,
-            spaceBetween: 0
-          },
-          0: {
-            loop: true,
-            slidesPerView: "auto",
-            spaceBetween: 15
-          }
-        },
+        loop: true,
+        loopedSlides: 3,
+        slidesPerView: "auto",
         navigation: {
           nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev"
-        }
+          prevEl: ".swiper-button-prev",
+        },
+        breakpoints: {
+          1024: {
+            spaceBetween: 0,
+          },
+          768: {
+            spaceBetween: 0,
+          },
+          0: {
+            spaceBetween: 0,
+          },
+        },
       },
       swiperOptionThumbs: {
         loop: true,
@@ -109,8 +129,8 @@ export default {
         slidesPerView: "auto",
         spaceBetween: 30,
         touchRatio: 0.2,
-        slideToClickedSlide: true
-      }
+        slideToClickedSlide: true,
+      },
     };
   },
   mounted() {},
@@ -124,15 +144,14 @@ export default {
           swiperThumbs.controller.control = swiperTop;
         });
       }
-    }
+    },
   },
-  methods: {
-  },
+  methods: {},
   computed: {
     data() {
       return this.$store.state.blocks.product_full;
-    }
-  }
+    },
+  },
 };
 </script>
 
